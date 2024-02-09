3e7f3b06027cd3e24c5f5a7987d41e2562cfd175
Kr-Gagandeo1025 <88143827+Kr-Gagandeo1025@users.noreply.github.com>
2024-02-09 15:57:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {Server} = require('socket.io');
+
+const io = new Server({
+    cors:true,
+});
+const app = express();
+
+app.use(bodyParser.json());
+
+const emailToSocketMap = new Map();
+const socketToEmailMap = new Map();
+
+
+//creating a socket ipc for connection to room
+io.on('connection', (socket) => {
+    console.log("new connection",socket.id);
+    socket.on("join-room",(data)=>{
+        console.log(data);
+        const {email, roomName} = data;
+        emailToSocketMap.set(email,socket.id);
+        socketToEmailMap.set(socket.id,email);
+        io.to(roomName).emit("new-user-joined",{email,id:socket.id});
+        socket.join(roomName);
+        io.to(socket.id).emit('join-room',data);
+    });
+
+    socket.on("user:call",({to,offer})=>{
+        io.to(to).emit('incoming:call',{from:socket.id,offer});
+    });
+
+    socket.on("call:accepted",({to,ans})=>{
+       io.to(to).emit("call:accepted",{from:socket.id, ans});
+    })
+
+    socket.on("peer:nego:needed",({to,offer})=>{
+        io.to(to).emit("peer:nego:needed",{from:socket.id, offer});
+    })
+
+    socket.on("peer:nego:done",({to,ans})=>{
+        io.to(to).emit("peer:nego:final",{from:socket.id, ans});
+    })
+});
+
+app.listen(8000,()=>{
+    console.log("Server running at port 8000");
+})
+io.listen(8001);
